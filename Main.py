7ae08543df62509d0f94be5c69a7f905b563baad
BoopYurNose <zacharybaca07@gmail.com>
2026-04-16 10:42:54 +0000

    :wq

--- Main.py
+++ Main.py
@@ -1,1 +1,15 @@
+import sqlite3
 
+# Connection Here! :3
+Connection = sqlite3.connect("BankingDatabase")
+Data = Connection.cursor()
+
+#Creating a table
+Data.execute('''CREATE TABLE IF NOT EXISTS users
+             (id INTEGER PRIMARY KEY, name TEXT NOT NULL, age INTEGER NOT NULL)''')
+
+# Inserting Data
+Data.execute("INSERT INTO users (name, age) VALUES ('Amelia', 22)")
+Connection.commit()
+
+Connection.close()
