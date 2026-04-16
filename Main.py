94640d7ea8c54e3fb469f250b6ba54766d96c894
BoopYurNose <zacharybaca07@gmail.com>
2026-04-16 17:02:08 +0000

    :wq

--- Main.py
+++ Main.py
@@ -1,21 +1,76 @@
-import sqlite3
+'''Project Requirements
+Your banking app must support:
 
-# Connection Here! :3
-Connection = sqlite3.connect("BankingDatabase")
-Data = Connection.cursor()
+Create new bank accounts (name, initial deposit)
+Deposit money into an account
+Withdraw money (with balance validation)
+Check account balance
+List / manage existing accounts
+A simple menu-driven interface (terminal UI)'''
+# This is my CLI if I finish this I will then make a web application version of this
 
-#Creating a table
-Data.execute('''CREATE TABLE IF NOT EXISTS users
-             (id INTEGER PRIMARY KEY, name TEXT NOT NULL, age INTEGER NOT NULL)''')
+# NOTE: have the sqlite3 database have all account credientials/balances/etc stored
 
-# Inserting Data
-Data.execute("INSERT INTO users (name, age) VALUES ('Amelia', 22)")
+# Everytime a user creates a new account store that accounts credientials in the database
+#  and have a default balance value of 0 unless they change it
 
-Connection.commit()
+# you should add a character limit to the inputs of money, username, password, etc so some jackass doesn't type like 1,000 characters
+# and mess up the formatting lmfao
 
-Data.execute("SELECT * FROM users")
-rows = Data.fetchall()
-for row in rows:
-    print(row)
+def LineFormat(AmountLines):
+    Lines = AmountLines
+    LineFormat = ""
+    for i in range(1, Lines):
+        LineFormat += "-"
 
-Connection.close()
+    print(LineFormat)
+
+def AccountCreationValidator(Name, Username = None, Password = None):
+    if not Username == None:
+        pass
+        # validate Username here
+
+    if not Password == None:
+        pass
+        # Validate the password here
+    
+    if Name.isalpha():
+        return True
+    else:
+        return False
+
+
+    
+
+def MainMenu():
+    LineFormat(50)
+    print("Welcome to my banking application! \n Would you like to either Sign in Type: SignIn \n Or would you like to Sign up Type: SignUp")
+
+    UserChoice = input(":")
+    if not UserChoice.lower() == "signin" and not UserChoice.lower() == "signup":
+        print("Try again you can only pick either SignIn or SignUp by typing those and pressing enter \n")
+        MainMenu()
+        return
+    elif UserChoice.lower() == "signin":
+        # have sign in thing here
+        pass
+    elif UserChoice.lower() == "signup":
+        LineFormat(50)
+        print("Welcome to the sign up page \n if for any reason you would like to return to the MainMenu Type: Exit \n to any of the prompts")
+        print("You will now input your name, username, password \n then after doing so you will be returned to the main menu")
+        print("Make sure to remember these login credientials that you create \n becuase you will need to use these to login")
+        print("After you've completed your account creation \n")
+
+        NameInput = input("Please input your name Only include your first name with no numbers:")
+
+        while not AccountCreationValidator(NameInput):
+            print("Try again you need to input your name with NO NUMBERS")
+            NameInput = input("Please input your name Only include your first name with no numbers:")
+        
+        UsernameInput = input("Please input your username now")
+
+
+    
+    
+MainMenu()
+
