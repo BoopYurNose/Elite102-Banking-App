3178a3441b7b9ea684d498efbeafc403ea892e5e
BoopYurNose <zacharybaca07@gmail.com>
2026-04-27 23:11:34 +0000

    :wq

--- PracticeDatabase.py
+++ PracticeDatabase.py
@@ -22,7 +22,31 @@ Cur = DatabaseConnection.cursor()
 '''
 
 # Delete Records
-Cur.execute("DELETE from people WHERE rowid = 6")
+#Cur.execute("DELETE from people WHERE rowid = 6")
+
+# ORDER BY
+#Cur.execute("SELECT rowid, * FROM people ORDER BY rowid") #NOTE if left ommitted (meaning no specification) it'll automatically just ascend
+#Cur.execute("SELECT rowid, * FROM people ORDER BY rowid DESC") descending order
+
+#Cur.execute("SELECT rowid, * FROM people ORDER BY LastName") # alphabetical order from A-Z
+#Cur.execute("SELECT rowid, * FROM people ORDER BY LastName DESC") #Same as previous but just other way around
+
+
+
+
+# Query The Database - AND/OR
+#Cur.execute("SELECT rowid, * FROM people WHERE LastName LIKE 'Am%' AND rowid = 1") #literally the same logic as the and and or operator in if statements in python
+#Cur.execute("SELECT rowid, * FROM people WHERE LastName LIKE 'Am%' OR rowid = 3")
+
+# Query The Database - LIMIT
+#Cur.execute("SELECT rowid, * FROM people LIMIT 2") just limits the amount of results you can get from a query
+#Cur.execute("SELECT rowid, * FROM people ORDER BY rowid DESC LIMIT 2")
+
+
+
+
+# Drop Table (How to delete an entire table)
+#Cur.execute("DROP TABLE people")
 
 Cur.execute("SELECT rowid, * FROM people")
 
