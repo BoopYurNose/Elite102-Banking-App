56f8ca7b2268864db1433cc73b38faef805cfc07
BoopYurNose <zacharybaca07@gmail.com>
2026-04-27 21:47:54 +0000

    :wq

--- PracticeDatabase.py
+++ PracticeDatabase.py
@@ -4,13 +4,29 @@ import sqlite3
 DatabaseConnection = sqlite3.connect("Customer.db")
 
 # Create a cursor
-Cursor = DatabaseConnection.cursor()
+Cur = DatabaseConnection.cursor()
 
 # Create a table
-Cursor.execute("""CREATE TABLE people (
-                   
-)
+Cur.execute("""CREATE TABLE IF NOT EXISTS people (
+        FirstName text,
+        LastName text,
+        Email text               
+    )""")
 
+# sqlite3 datatypes
+'''
+NULL
+INTEGER
+REAL
+TEXT
+BLOB
+'''
 
+Cur.execute("INSERT INTO people VALUES ('Pumpkin', 'Cat', 'kittycat@gmail.com')")
 
-    """)
+print("Successfully ran!")
+# Commit our command
+DatabaseConnection.commit()
+
+# Close the connection
+DatabaseConnection.close()
